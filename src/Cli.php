e24259574c0926823c43e67b84f33217e5a603be
Maksim Litvinov <litvinovmksm@gmail.com>
2019-12-12 19:00:59 +0300

    Add format

--- src/Cli.php
+++ src/Cli.php
@@ -12,10 +12,12 @@ function run()
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
+      gendiff [--format <fmt>] <file1> <file2>
 
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
+      --format <fmt>                Report format [default: pretty]
     DOC;
 
     $params = [
