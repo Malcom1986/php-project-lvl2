feeb61517d340760065b0d4744f11baa6ac89ae5
Maksim Litvinov <litvinovmksm@gmail.com>
2020-01-28 20:03:46 +0300

    fix pretty formatter

--- src/Formatters/PrettyFormatter.php
+++ src/Formatters/PrettyFormatter.php
@@ -3,68 +3,94 @@
 namespace Differ\Formatters\PrettyFormatter;
 
 use function Differ\Node\{getName, getType, getNewValue, getOldValue, getChildren};
+use function Funct\Strings\times;
+
+const START_OFFSET = 2;
 
 function formatPretty($tree)
 {
     $formatters = [
-        'added' => function ($acc, $node) {
-            $key = getName($node);
+        'added' => function ($acc, $node, $offset) {
+            $name = getName($node);
             $newValue = getNewValue($node);
-            $formatedNewValue = is_object($newValue) ? formatObject($newValue) : $newValue;
-            $acc["+ {$key}"] = $formatedNewValue;
+            $formatedNewValue = stringifyValue($newValue, $offset);
+            $indention = times(' ', $offset + START_OFFSET);
+            $acc[] = "{$indention}+ {$name}: {$formatedNewValue}";
             return $acc;
         },
-        'deleted' => function ($acc, $node) {
-            $key = getName($node);
+        'deleted' => function ($acc, $node, $offset) {
+            $name = getName($node);
             $oldValue = getOldValue($node);
-            $formatedOldValue = is_object($oldValue) ? formatObject($oldValue) : $oldValue;
-            $acc["- {$key}"] = $formatedOldValue;
+            $formatedOldValue = stringifyValue($oldValue, $offset);
+            $indention = times(' ', $offset + START_OFFSET);
+            $acc[] = "{$indention}- {$name}: {$formatedOldValue}";
             return $acc;
         },
-        'unchanged' => function ($acc, $node) {
-            $key = getName($node);
+        'unchanged' => function ($acc, $node, $offset) {
+            $name = getName($node);
             $oldValue = getOldValue($node);
-            $formatedOldValue = is_object($oldValue) ? formatObject($oldValue) : $oldValue;
-            $acc["  {$key}"] = $formatedOldValue;
+            $formatedOldValue = stringifyValue($oldValue, $offset);
+            $indention = times(' ', $offset + START_OFFSET);
+            $acc[] = "{$indention}  {$name}: {$formatedOldValue}";
             return $acc;
         },
-        'changed' => function ($acc, $node) {
-            $key = getName($node);
+        'changed' => function ($acc, $node, $offset) {
+            $name = getName($node);
+            $indention = times(' ', $offset + START_OFFSET);
             $oldValue = getOldValue($node);
-            $formatedOldValue = is_object($oldValue) ? formatObject($oldValue) : $oldValue;
+            $formatedOldValue = stringifyValue($oldValue, $offset);
             $newValue = getNewValue($node);
-            $formatedNewValue = is_object($newValue) ? formatObject($newValue) : $newValue;
-            $acc["- {$key}"] = $formatedOldValue;
-            $acc["+ {$key}"] = $formatedNewValue;
+            $formatedNewValue = stringifyValue($newValue, $offset);
+            $acc[] = "{$indention}+ {$name}: {$formatedNewValue}";
+            $acc[] = "{$indention}- {$name}: {$formatedOldValue}";
             return $acc;
         },
-        'nested' => function ($acc, $node, $func) {
-            $key = getName($node);
+        'nested' => function ($acc, $node, $offset, $func) {
+            $name = getName($node);
+            $indention = times(' ', $offset + START_OFFSET);
             $children = getChildren($node);
-            $acc["  {$key}"] = $func($children);
+            $offset += 4;
+            $formatedChildren = $func($children, $offset);
+            $acc[] = "{$indention}  {$name}: {$formatedChildren}";
             return $acc;
         }
     ];
 
-    $formatPretty = function ($tree) use (&$formatPretty, $formatters) {
-        return array_reduce($tree, function ($acc, $node) use ($formatters, $formatPretty) {
+    $formatPretty = function ($tree, $offset = 0) use (&$formatPretty, $formatters) {
+        $formatted = array_reduce($tree, function ($acc, $node) use ($formatters, $formatPretty, $offset) {
             $nodeType = getType($node);
             $formatNode = $formatters[$nodeType];
-            return $formatNode($acc, $node, $formatPretty);
+            return $formatNode($acc, $node, $offset, $formatPretty);
         }, []);
+        $string = implode("\n", $formatted);
+        $indention = times(' ', $offset);
+        return "{\n{$string}\n{$indention}}";
     };
 
-    $formatedTree = $formatPretty($tree);
-    $json = json_encode($formatedTree, JSON_PRETTY_PRINT);
-    return str_replace(['"', ','], '', $json);
+    return $formatPretty($tree);
 }
 
-function formatObject($object)
+function stringifyValue($value, $offset)
 {
-    return collect($object)->mapWithKeys(function ($value, $key) {
-        if (is_array($value)) {
-            return ["  {$key}" => formatObject($value)];
-        }
-        return ["  {$key}" => $value];
+    $valueType = \gettype($value);
+    switch ($valueType) {
+        case 'object':
+            return stringifyObject($value, $offset);
+        case 'boolean':
+            return json_encode($value);
+        case 'array':
+            return implode(',', $value);
+        default:
+            return $value;
+    }
+}
+
+function stringifyObject($object, $offset)
+{
+    $indention = times(' ', $offset + 4);
+    $formated = collect($object)->map(function ($value, $key) use ($indention) {
+        return "{$indention}   {$key}: {$value}";
     })->all();
+    $string = implode("\n", $formated);
+    return "{\n{$string}\n{$indention}}";
 }
